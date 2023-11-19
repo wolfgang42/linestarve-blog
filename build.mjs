81921e8f3604977a531fe15de2f97caf80adf68c
Wolfgang Faust <wolfgang42@git>
2023-11-19 10:22:45 -0800

    refactor: rearrange post reader

--- build.mjs
+++ build.mjs
@@ -11,6 +11,10 @@ markdown.use(MarkdownItHighlightjs, {auto: false, code: false})
 
 const DATE_FORMATTER = new Intl.DateTimeFormat('en-US', {dateStyle: 'long'})
 const urlize = str => str.replaceAll(/ /g, '-').replaceAll(/[:'<>]/g, '')
+const RENDER_TYPE = {
+	html: text => text,
+	md: text => markdown.render(text),
+}
 
 await fs.rm('./public', {force: true, recursive: true})
 await fs.mkdir('./public')
@@ -19,71 +23,79 @@ await fs.mkdir('./public/blog/post')
 await fs.cp('./themes/startbootstrap-clean-blog/static/', './public/blog/', {recursive: true})
 await fs.cp('./static/', './public/blog/', {recursive: true})
 
+async function read_post_entry(filename) {
+	const filematch = filename.match(/^(?<date>[0-9]{4}-[0-9]{2}-[0-9]{2})-(?<slug>[^.]+)(?:\.(?<ext>html|md))?$/)
+	if (!filematch) throw new Error('failed to match on post name')
+	const {date, slug, ext} = filematch.groups
+	
+	let postfile = `${postdir}/${filename}`
+	let assets = []
+	if (ext === undefined) {
+		let found = false
+		for (const subfile of await fs.readdir(postfile)) {
+			if (subfile === `${date}-${slug}.md` || subfile === `${date}-${slug}.html`) {
+				if (found) throw new Error('Found multiple post files')
+				postfile += '/'+subfile
+				found = true
+			} else {
+				assets.push({path: `${postdir}/${filename}/${subfile}`, filename: subfile})
+			}
+		}
+		if (!found) throw new Error('Failed to find post file')
+	}
+	const filetype = postfile.split('.').at(-1)
+	if (!(filetype in RENDER_TYPE)) throw new Error('unknown file type')
+	
+	return {date, slug, filetype, filename: postfile, assets}
+}
+
+async function parse_post(post) {
+	const lines = (await fs.readFile(post.filename, 'utf-8')).split('\n')
+	let mode = 'start', frontmatter_raw = '', body_truncated = '', body = ''
+	for (const line of lines) {
+		if (mode === 'start') {
+			if (line !== '---') throw new Error('missing front matter start indicator')
+			mode = 'frontmatter'
+		} else if (mode === 'frontmatter') {
+			if (line === '---') {
+				mode = 'body'
+			} else {
+				frontmatter_raw += line+'\n'
+			}
+		} else if (mode === 'body') {
+			if (line === '<!--more-->') {
+				body += '\n'
+				mode = 'body_truncated'
+			} else {
+				body += line+'\n'
+				body_truncated += line+'\n'
+			}
+		} else if (mode === 'body_truncated') {
+			body += line+'\n'
+		} else {
+			throw new Error("can't happen")
+		}
+	}
+	if (mode !== 'body' && mode !== 'body_truncated') throw new Error('failed to enter body mode')
+	
+	const frontmatter = Yaml.parse(frontmatter_raw)
+	if ((!frontmatter.short) !== (mode === 'body_truncated')) throw new Error('short flag does not match truncation')
+	const render = RENDER_TYPE[post.filetype]
+	
+	return {
+		...post,
+		tags: [], // TODO default in case frontmatter does not define
+		...frontmatter,
+		htmlBody: render(body),
+		htmlSummary: render(body_truncated),
+	}
+}
+
 const postdir = './posts'
 const posts = await Promise.all((await fs.readdir(postdir))
 .map(async filename => {
 	try {
-		const filematch = filename.match(/^(?<date>[0-9]{4}-[0-9]{2}-[0-9]{2})-(?<slug>[^.]+)(?:\.(?<ext>html|md))?$/)
-		if (!filematch) throw new Error('failed to match on post name')
-		const {date, slug, ext} = filematch.groups
-		
-		await fs.mkdir(`public/blog/post/${slug}`, {recursive: true}) // NOTE: recursive only to suppress EEXIST
-		
-		let postfile = `${postdir}/${filename}`
-		if (ext === undefined) {
-			let found = false
-			for (const subfile of await fs.readdir(postfile)) {
-				if (subfile === `${date}-${slug}.md` || subfile === `${date}-${slug}.html`) {
-					if (found) throw new Error('Found multiple post files')
-					postfile += '/'+subfile
-					found = true
-				} else {
-					await fs.cp(`${postdir}/${filename}/${subfile}`, `public/blog/post/${slug}/${subfile}`, {recursive: true, preserveTimestamps: true})
-				}
-			}
-			if (!found) throw new Error('Failed to find post file')
-		}
-		const lines = (await fs.readFile(postfile, 'utf-8')).split('\n')
-		let mode = 'start', frontmatter_raw = '', body_truncated = '', body = ''
-		for (const line of lines) {
-			if (mode === 'start') {
-				if (line !== '---') throw new Error('missing front matter start indicator')
-				mode = 'frontmatter'
-			} else if (mode === 'frontmatter') {
-				if (line === '---') {
-					mode = 'body'
-				} else {
-					frontmatter_raw += line+'\n'
-				}
-			} else if (mode === 'body') {
-				if (line === '<!--more-->') {
-					body += '\n'
-					mode = 'body_truncated'
-				} else {
-					body += line+'\n'
-					body_truncated += line+'\n'
-				}
-			} else if (mode === 'body_truncated') {
-				body += line+'\n'
-			} else {
-				throw new Error("can't happen")
-			}
-		}
-		if (mode !== 'body' && mode !== 'body_truncated') throw new Error('failed to enter body mode')
-		
-		const frontmatter = Yaml.parse(frontmatter_raw)
-		if ((!frontmatter.short) !== (mode === 'body_truncated')) throw new Error('short flag does not match truncation')
-		const post = {
-			slug,
-			date,
-			tags: [], // TODO ditto default
-			...frontmatter,
-			filename: postfile,
-			htmlBody: (ext === 'html') ? body : markdown.render(body),
-			htmlSummary: (ext === 'html') ? body_truncated : markdown.render(body_truncated),
-		}
-		
-		return post
+		return parse_post(await read_post_entry(filename))
 	} catch (e) {
 		console.error(`error in ${filename}:`)
 		throw e
@@ -111,7 +123,11 @@ for (const post of posts) {
 		...templateGlobals,
 	})
 	
+	await fs.mkdir(`public/blog/post/${post.slug}`, {recursive: true}) // NOTE: recursive only to suppress EEXIST
 	await fs.writeFile(`public/blog/post/${post.slug}/index.html`, html)
+	for (const asset of post.assets) {
+		await fs.cp(asset.path, `public/blog/post/${post.slug}/${asset.filename}`, {recursive: true, preserveTimestamps: true})
+	}
 }
 
 for (const [tag, posts] of Object.entries(posts_by_tag)) {
